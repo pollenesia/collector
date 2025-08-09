ae711f0ce063a3a73605be149e9ffcd450fe202c
ri <satviewer@ya.ru>
2025-08-09 17:01:01 +0300

    .

--- postprocessing.py
+++ postprocessing.py
@@ -7,14 +7,37 @@ from utils import get_logger, get_file_list
 import argparse
 import numpy as np
 import os
-
+import shutil
 
 logger = get_logger(__name__)
 
 
+def get_image_fname(fname: str):
+    path = os.path.dirname(fname)
+    f = File(fname, 'r')
+    d = f['data']
+    header = list(f['data'].attrs['header'])
+    header: list
+    i_sharpness = header.index('Sharpness')
+    i_max_sharpness = np.argmax(d[:, i_sharpness])
+    image_fname = f'image{i_max_sharpness:03d}.jpg'
+    image_path = os.path.join(path, image_fname)
+    logger.info(image_path)
+    f.close()
+    return image_path
+
+
+def copy_images(fnames: list[str]):
+    for fname in fnames:
+        dirname = os.path.basename(os.path.dirname(fname))
+        dst = os.path.join('data', 'filtered', f'{dirname}.jpg')
+        logger.info(dst)
+        shutil.copyfile(fname, dst)
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('path', help='path list', type=str, nargs='*')
+    parser.add_argument('path', help='path', type=str)
 
     try:
         options, _ = parser.parse_known_args()
@@ -23,17 +46,22 @@ def main():
         logger.error(e)
         exit(0)
 
-    for i, path in enumerate(reversed(options.path)):
-        pattern = os.path.join(path, '**', 'stat.txt')
-        flist = get_file_list(pattern, recursive=True)
+    # os.path.join(options.path, '**', 'data.h5')
+    pattern = options.path
+    flist = get_file_list(pattern, recursive=True)
+    image_list = []
 
-        for fname in flist:
-            logger.info(fname)
-            path0 = os.path.dirname(fname)
-            rcv_name = load_replies(path0).lower()
-            label = f'{i + 1}_{rcv_name}'
-            d = read_stat_result(fname)
-            write_result_json(path0, d)
+    for fname in flist:
+        logger.info(fname)
+        image = get_image_fname(fname)
+        image_list.append(image)
+
+    copy_images(image_list)
+
+    # f = open('list.txt', 'wt')
+    # for line in image_list:
+    #     f.write(f'{line}\n')
+    # f.close()
 
 
 if __name__ == '__main__':
