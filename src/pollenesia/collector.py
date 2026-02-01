e3ee1fc1edcf768c3ccae234315c1a3471783d62
pollenesia <pollenesiaproject@gmail.com>
2026-02-02 00:32:40 +0300

    .

--- src/pollenesia/collector.py
+++ src/pollenesia/collector.py
@@ -31,6 +31,7 @@ THREAD_N = THREAD_LENGTH_MM / THREAD_STEP_MM
 MAX_STEPS = int(512.0 * THREAD_N)
 
 STEP_WAIT = 0.002
+DATA_CALLBACK = {}
 
 
 def get_logger(name: str):
@@ -52,32 +53,32 @@ PIN_MOTOR2 = [6, 13, 19, 26]
 FORWARD = False
 BACKWARD = True
 
-motor1 = rml.BYJMotor("motor1", "28BYJ")
-motor2 = rml.BYJMotor("motor2", "28BYJ")
-is_home = False
-camera = None  # Picamera2()
-position_step = 0
 
+def go_home(din: dict) -> dict:
+    dout = {}
+    motor = din['motor_camera']
 
-def go_home() -> bool:
-    global is_home
-    global position_step
-    is_home = False
     if gpio.input(PIN_STOP) == True:
         logger.info('moving to home')
-        motor1.motor_run(PIN_MOTOR1, STEP_WAIT, MAX_STEPS,
-                         BACKWARD, False, "full", 0.05)
-        position_step = 0
-    return gpio.input(PIN_STOP) == False
+        motor.motor_run(PIN_MOTOR1, STEP_WAIT, MAX_STEPS,
+                        BACKWARD, False, "full", 0.05)
+        dout['position_step'] = 0
+
+    dout['is_home'] = gpio.input(PIN_STOP) == False
+    dout['command'] = ''
+    return dout
 
 
-def go_end() -> bool:
-    global is_home
+def go_end(din: dict) -> dict:
+    dout = {}
+    motor = din['motor_camera']
+    is_home = din['is_home']
     if is_home:
         logger.info('moving to end')
-        motor1.motor_run(PIN_MOTOR1, STEP_WAIT, MAX_STEPS,
-                         FORWARD, False, "full", 0.05)
-    return is_home
+        motor.motor_run(PIN_MOTOR1, STEP_WAIT, MAX_STEPS,
+                        FORWARD, False, "full", 0.05)
+    dout['command'] = ''
+    return dout
 
 
 def go_steps(steps: int) -> int:
@@ -112,33 +113,34 @@ def leave_home() -> bool:
 
 
 def event_stop(channel: int):
-    global is_home
-    is_released = gpio.input(PIN_STOP)
-    if is_home and is_released:
-        logger.info('home leaved')
-    elif is_home and not is_released:
-        motor1.motor_stop()
-        logger.info('home reached')
-    elif not is_home and is_released:
-        motor1.motor_stop()
-        logger.info('limit switch released')
-    else:
-        motor1.motor_stop()
-        logger.info('home fixed')
-        is_home = True
-    # logger.info(f'PIN_STOP[{PIN_STOP}] is {is_released}')
+    global DATA_CALLBACK
+    motor = DATA_CALLBACK['motor_camera']
+    is_home = DATA_CALLBACK['is_home']
+    try:
+        is_released = gpio.input(PIN_STOP)
+        if is_home and is_released:
+            logger.info('home leaved')
+        elif is_home and not is_released:
+            motor.motor_stop()
+            logger.info('home reached')
+        elif not is_home and is_released:
+            motor.motor_stop()
+            logger.info('limit switch released')
+        else:
+            motor.motor_stop()
+            logger.info('home fixed')
+    except Exception as e:
+        logger.error(e)
+        exit(-1)
 
 
 def init_camera():
-    global camera
-    if camera is not None:
-        return
-    logger.info('Camera configuring')
     camera = Picamera2()
-    config = camera.create_still_configuration({"size": (1024, 1024)})
+    config = camera.create_still_configuration({'size': (1024, 1024)})
     camera.configure(config)
     camera.start()
     time.sleep(2.0)
+    return camera
 
 
 def get_image(fname: str) -> dict[str, any]:
@@ -178,12 +180,28 @@ def release_break(release: bool):
     logger.info(f'break release set to {release}')
 
 
-def init():
-    logger.info('hello')
+def init() -> dict:
+    global DATA_CALLBACK
+    data = {}
+
+    logger.info('motors')
     gpio.setmode(gpio.BCM)
     gpio.setup(PIN_STOP, gpio.IN, gpio.PUD_UP)
     gpio.setup(PIN_BREAK, gpio.OUT, initial=gpio.LOW)
     gpio.add_event_detect(PIN_STOP, gpio.BOTH, event_stop, 200)
+
+    data['motor_camera'] = rml.BYJMotor('camera', '28BYJ')
+    data['motor_tape'] = rml.BYJMotor('tape', '28BYJ')
+
+    logger.info('camera')
+    # data['camera'] = init_camera()
+
+    logger.info('variables')
+    data['is_home'] = False
+    data['position_step'] = 0
+    data['command'] = ''
+    DATA_CALLBACK = data
+    return data
 
 
 def test_passing_with_brake():
@@ -202,9 +220,13 @@ def on_message(client, userdata, msg):
     command = data['command']
     if command == 'release-break':
         release_break(data['value'])
+    elif command == 'home':
+        userdata['command'] = 'go_home'
+    elif command == 'end':
+        userdata['command'] = 'go_end'
 
 
-def init_mqtt() -> mqtt.Client:
+def init_mqtt(userdata) -> mqtt.Client:
     client = mqtt.Client(
         callback_api_version=mqtt.CallbackAPIVersion.VERSION2,
         protocol=mqtt.MQTTv5,
@@ -221,8 +243,7 @@ def init_mqtt() -> mqtt.Client:
 
     client.subscribe('pollenesia/collector')
     client.on_message = on_message
-
-    # client.loop_start()
+    client.user_data_set(userdata)
 
     return client
 
@@ -248,26 +269,26 @@ def main():
     if not os.path.exists(options.output):
         os.makedirs(options.output)
 
-    init()
-    mqtt_client = init_mqtt()
-    mqtt_client.loop_forever()
-    exit(0)
+    data = init()
+    mqtt_client = init_mqtt(data)
 
-    test_passing_with_brake()
+    mqtt_client.loop_start()
+    # mqtt_client.loop_forever()
 
-    is_home = True
-    go_end()
+    # test_passing_with_brake()
+    # is_home = True
+    # go_end()
 
     dkeys = [
         'SensorTimestamp', 'ExposureTime',
         'FocusFoM', 'Lux', 'Sharpness', 'Position', 'Position_mm',
     ]
 
-    mode = 'm'
+    mode = 'manual'
     m_step = 100
     m_pass_step = 100
     while True:
-        if mode == 'a':
+        if mode == 'auto':
             pass_tape(64)
             date = datetime.now()
             dirname = f'{(date.year % 100):02d}{date.month:02d}{date.day:02d}{date.hour:02d}{date.minute:02d}{date.second:02d}'
@@ -285,20 +306,20 @@ def main():
                 logger.error('limit switch stuck')
                 exit(-1)
 
-        data = np.ndarray((0, len(dkeys)))
-        logger.info(data.shape)
+        img_data = np.ndarray((0, len(dkeys)))
+        logger.info(img_data.shape)
         steps = 0
         i_image = 0
         while True:
             try:
-                if mode == 'a':
+                if mode == 'auto':
                     position_mm = steps / 512.0 * THREAD_STEP_MM
                     if position_mm < 15.0:
                         steps = go_steps(512)
                     elif position_mm >= 20.0:
                         logger.info(f'storing {dirname}/data.h5')
                         f = File(f'{dirname}/data.h5', 'w')
-                        f['data'] = data
+                        f['data'] = img_data
                         f['data'].attrs['header'] = dkeys
                         f.close()
                         dt = (datetime.now() - date).seconds
@@ -314,33 +335,41 @@ def main():
                         # time.sleep(0.2)
                         row = get_data_row(
                             f'{dirname}/image{i_image:03d}.jpg', dkeys)
-                        data = np.vstack((data, row))
+                        img_data = np.vstack((img_data, row))
                         i_image += 1
-                else:
-                    for line in sys.stdin:
-                        line = line.rstrip()
-                        if line == 'f':
-                            motor1.motor_run(PIN_MOTOR1, STEP_WAIT, m_step,
-                                             FORWARD, False, "full", 0.05)
-                        elif line == 'b':
-                            motor1.motor_run(PIN_MOTOR1, STEP_WAIT, m_step,
-                                             BACKWARD, False, "full", 0.05)
-                        elif line == 'i':
-                            m_step *= 10
-                            logger.info(f'step: {m_step}')
-                        elif line == 'd':
-                            m_step /= 10
-                            m_step = 1 if m_step < 1 else m_step
-                            logger.info(f'step: {m_step}')
-                        elif line == 'p':
-                            pass_tape(m_pass_step)
-                        elif line == '>':
-                            m_pass_step *= 2
-                            logger.info(f'step: {m_pass_step}')
-                        elif line == '<':
-                            m_pass_step /= 2
-                            m_pass_step = 1 if m_pass_step < 1 else m_pass_step
-                            logger.info(f'step: {m_pass_step}')
+                elif mode == 'manual':
+                    command = data['command']
+                    if command == 'go_home':
+                        d = go_home(data)
+                        data.update(d)
+                    elif command == 'go_end':
+                        d = go_end(data)
+                        data.update(d)
+                    time.sleep(0.1)
+                    # for line in sys.stdin:
+                    #     line = line.rstrip()
+                    #     if line == 'f':
+                    #         motor1.motor_run(PIN_MOTOR1, STEP_WAIT, m_step,
+                    #                          FORWARD, False, "full", 0.05)
+                    #     elif line == 'b':
+                    #         motor1.motor_run(PIN_MOTOR1, STEP_WAIT, m_step,
+                    #                          BACKWARD, False, "full", 0.05)
+                    #     elif line == 'i':
+                    #         m_step *= 10
+                    #         logger.info(f'step: {m_step}')
+                    #     elif line == 'd':
+                    #         m_step /= 10
+                    #         m_step = 1 if m_step < 1 else m_step
+                    #         logger.info(f'step: {m_step}')
+                    #     elif line == 'p':
+                    #         pass_tape(m_pass_step)
+                    #     elif line == '>':
+                    #         m_pass_step *= 2
+                    #         logger.info(f'step: {m_pass_step}')
+                    #     elif line == '<':
+                    #         m_pass_step /= 2
+                    #         m_pass_step = 1 if m_pass_step < 1 else m_pass_step
+                    #         logger.info(f'step: {m_pass_step}')
             except KeyboardInterrupt:
                 logger.info('exit')
 
